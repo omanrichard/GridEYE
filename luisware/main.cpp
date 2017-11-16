fccca635af7d410cc9f20f8024da034a611ad3e2
Grant Hilgert <Grant@Grants-MacBook-Pro.local>
2017-11-15 21:16:05 -0600

    define GridEye class

--- luisware/main.cpp
+++ luisware/main.cpp
@@ -23,14 +23,80 @@
 #include <vector>
 #include <string>
 
+#define GRIDEYEADDRESS 0x68
+
+
+class GridEYE{
+    private:
+    
+    public:
+    GridEYE(int address); //Hint: its at 0x68
+    int read(int row, int col);
+    void reset(void);
+    int test(int row, int col);//Draw Test pattern
+};
+
+GridEYE::GridEYE(int address){
+    /*/
+     I2C begin transmission @ address
+     //Inisilize sensor
+    /*/
+}
+int GridEYE::read(int row, int col){
+    /*/ read code /*/
+}
+void GridEYE::reset(void){
+
+}
+int GridEYE::test(int row, int col){
+    return row*col*4;
+    
+}
+
+
+/*/
+class terminal {
+    private:
+    std::string Row1;
+    std::string Row2;
+    std::string Row3;
+    std::string Row4;
+    std::string Row5;
+    std::string Row6;
+    std::string Row7;
+    
+    public:
+    terminal(int x1,int y1,int length,int width);
+    void write(std::string newText);
+    void clear(void);
+    void draw(void);
+};
+//length = 625, width = 94, x1 = 94, y1 = 596
+terminal::terminal(int x1, int y1, int length, int width){
+    sf::RectangleShape terminal(sf::Vector2f( length, width ));
+    terminal.setFillColor(sf::Color::Black);
+    terminal.setPosition(x1, y1+10);
+    
+    sf::RectangleShape terminalHeader(sf::Vector2f( length, 10 ));
+    terminalHeader.setFillColor(sf::Color(255,94,20));
+    terminalHeader.setPosition(x1, y1);
+}
+void terminal::draw(void){
+    window.draw(terminalHeader);
+    window.draw(terminal);
+}
+/*/
 
 
 using namespace sf;
+GridEYE gridward(GRIDEYEADDRESS);
 
 // Here is a small helper for you! Have a look.
 //#include "ResourcePath.hpp"
 
 int i,j;
+
+
 
 int main(int, char const**)
 {
@@ -114,7 +180,7 @@ int main(int, char const**)
             gridy = (88+j*59);
             RectangleShape newPix(sf::Vector2f(58, 58));
             newPix.setPosition( gridx, gridy );
-            newPix.setFillColor(sf::Color::White);
+            newPix.setFillColor(sf::Color(0,gridward.test(i,j),0));
             grid[i][j] = newPix;
         }
     }
@@ -167,18 +233,13 @@ int main(int, char const**)
     
  /*/-----------------Draw Terminal Window-------------------/*/
     
-    sf::RectangleShape terminal(sf::Vector2f( 625, 94 ));
-    terminal.setFillColor(sf::Color::Black);
-    terminal.setPosition(94, 606);
     
-    sf::RectangleShape terminalHeader(sf::Vector2f( 625, 10 ));
-    terminalHeader.setFillColor(sf::Color(255,94,20));
-    terminalHeader.setPosition(94, 596);
     
+    std::string terminalTextArray[6];
     
     sf::Text terminalText1("root@thermalCamera: Terminal text placeholder line 1", font, 12);
     terminalText1.setFillColor(sf::Color(0,255,0));
-    terminalText1.setPosition( 100 , 610);
+    terminalText1.setPosition( 100 , 608);
     
     sf::Text terminalText2("root@thermalCamera: Terminal text placeholder line 2", font, 12);
     terminalText2.setFillColor(sf::Color(0,255,0));
@@ -186,23 +247,28 @@ int main(int, char const**)
     
     sf::Text terminalText3("root@thermalCamera: Terminal text placeholder line 3", font, 12);
     terminalText3.setFillColor(sf::Color(0,255,0));
-    terminalText3.setPosition( 100 , 634);
+    terminalText3.setPosition( 100 , 636);
     
     sf::Text terminalText4("root@thermalCamera: Terminal text placeholder line 4", font, 12);
     terminalText4.setFillColor(sf::Color(0,255,0));
-    terminalText4.setPosition( 100 , 646);
+    terminalText4.setPosition( 100 , 650);
     
     sf::Text terminalText5("root@thermalCamera: Terminal text placeholder line 5", font, 12);
     terminalText5.setFillColor(sf::Color(0,255,0));
-    terminalText5.setPosition( 100 , 658);
+    terminalText5.setPosition( 100 , 664);
     
     sf::Text terminalText6("root@thermalCamera: Terminal text placeholder line 6", font, 12);
     terminalText6.setFillColor(sf::Color(0,255,0));
-    terminalText6.setPosition( 100 , 670);
+    terminalText6.setPosition( 100 , 678);
     
-    sf::Text terminalText7("root@thermalCamera: Terminal text placeholder line 7", font, 12);
-    terminalText7.setFillColor(sf::Color(0,255,0));
-    terminalText7.setPosition( 100 , 682);
+    sf::RectangleShape terminal(sf::Vector2f( 625, 94 ));
+    terminal.setFillColor(sf::Color::Black);
+    terminal.setPosition(94, 606);
+    
+    sf::RectangleShape terminalHeader(sf::Vector2f( 625, 10 ));
+    terminalHeader.setFillColor(sf::Color(255,94,20));
+    terminalHeader.setPosition(94, 596);
+   
     
     
     /*/---------------Background---------------/*/
@@ -212,6 +278,14 @@ int main(int, char const**)
     background.setFillColor(sf::Color(130, 117, 135));
     background.setPosition(0, 0);
     
+    sf::RectangleShape backgroundHeader(sf::Vector2f( 400, 10 ));
+    backgroundHeader.setFillColor(sf::Color(0, 255, 0));
+    backgroundHeader.setPosition(94, 70);
+    
+    sf::RectangleShape backgroundHeader2(sf::Vector2f( 400, 10 ));
+    backgroundHeader2.setFillColor(sf::Color(0, 255, 0));
+    backgroundHeader2.setPosition(488, 75);
+    backgroundHeader2.rotate(-60);
     
     sf::Text text("Thermal Camera", font, 50);
     text.setFillColor(sf::Color::Black);
@@ -267,16 +341,33 @@ int main(int, char const**)
         //Draw output Grid
         for( i=0 ; i < 8 ; i++ ){
             for( j=0 ; j<8 ; j++ ){
-                window.draw( grid[i][j] );
-            }
+                window.draw(grid[i][j]);            }
         }
         
         // Draw the placeholder text
+        window.draw(backgroundHeader);
+        window.draw(backgroundHeader2);
         window.draw(text);
         window.draw(r_text);
         
         
         //Draw the Terminal window
+        std::string newText;
+        
+        for(int i = 0; i < 5; i++){
+            terminalTextArray[i] = terminalTextArray[i+1];
+        }
+        terminalTextArray[5] = newText;
+        
+        terminalText1.setString(terminalTextArray[0]);
+        terminalText2.setString(terminalTextArray[1]);
+        terminalText3.setString(terminalTextArray[2]);
+        terminalText4.setString(terminalTextArray[3]);
+        terminalText5.setString(terminalTextArray[4]);
+        terminalText6.setString(terminalTextArray[5]);
+        
+        
+        
         window.draw(terminalHeader);
         window.draw(terminal);
         window.draw(terminalText1);
@@ -285,7 +376,7 @@ int main(int, char const**)
         window.draw(terminalText4);
         window.draw(terminalText5);
         window.draw(terminalText6);
-        window.draw(terminalText7);
+       
         
         
         
@@ -297,4 +388,21 @@ int main(int, char const**)
     }
 
     return EXIT_SUCCESS;
+}
+
+void terminalWrite(std::string newText){
+    
+ 
+    
+    
+sf::Font font;
+if (!font.loadFromFile("sansation.ttf")) {
+    return EXIT_FAILURE;
+}
+    
+    
+   
+    
+    
+    
 }
