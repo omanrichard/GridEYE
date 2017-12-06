faef06834aecc8c87a7439a80458220a60076237
Richard A. Oman <richard.oman@me.com>
2017-12-06 17:41:35 -0600

    test

--- GridEYE/projectClasses.cpp
+++ GridEYE/projectClasses.cpp
@@ -59,7 +59,7 @@ float GridEYE::read( int pixAddr ){
     temp = wiringPiI2CReadReg8( fd, pixAddr );  // Receive value from pixel
     wiringPiI2CWriteReg8( fd, pixAddr, 0);
                                                 // Pixels have 12-bit data
-    temp = (temp & 0x0800);
+    temp = (temp & 0x08FF);
     
     while( temp > 0x00 ){
         temp -= 0x04;
